8f234d1a3aa2f6e8199c54fe6ffad33b5b3e1dc0
Youssef Khaled <youssefkfarouk@gmail.com>
2022-04-29 20:46:50 +0200

    Time: 40 ms (78.14%) - Memory Usage: 27.6 MB (12.99%)

---
+++ Binary-Search/704.binary-search.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=704 lang=cpp
+ *
+ * [704] Binary Search
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0;
+        int end = nums.size() - 1;
+        while (start <= end){
+            int mid = (start + end) / 2;
+            if (target == nums[mid])
+                return mid;
+            else if (target < nums[mid])
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        return -1;
+    }
+};
+// @lc code=end
+
