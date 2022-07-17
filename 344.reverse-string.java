570850ac40691bf4273bca3962727cb81158df10
Youssef Khaled <youssefkfarouk@gmail.com>
2022-07-17 05:40:37 +0200

    Time: 1 ms(99.91%) - Memory Usage: 58 MB(15%)

---
+++ 344.reverse-string.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=344 lang=java
+ *
+ * [344] Reverse String
+ */
+
+// @lc code=start
+class Solution {
+    public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length - 1;
+
+        while(start < end){
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
+// @lc code=end
+
