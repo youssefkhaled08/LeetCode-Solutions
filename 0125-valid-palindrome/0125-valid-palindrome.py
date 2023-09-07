7ef485c9fcdeadffcab9e3260be2d5d7baeb85f0
Youssef Khaled <youssefkfarouk@gmail.com>
2023-09-07 23:47:14 +0300

    Time: 29 ms (73.14%), Space: 14 MB (77.38%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        left = 0
+        right = len(s) - 1
+        
+        while left <= right:
+            if not s[left].isalpha() and not s[left].isdigit():
+                left += 1
+            elif not s[right].isalpha() and not s[right].isdigit():
+                right -= 1
+            else:
+                if s[left].lower() != s[right].lower():
+                    return False
+                left += 1
+                right -= 1
+                
+        return True
