dbbd0a175cf9bd1a1ef53d7f04d8f1474cf15731
Youssef Khaled <youssefkfarouk@gmail.com>
2025-04-28 13:01:24 +0300

    Time: 0 ms (100.00%) | Memory: 46.4 MB (8.61%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,36 +1,35 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int[] ans = {-1, -1};
+        
+        ans[0] = findBound(nums, target, true);   // find left bound
+        ans[1] = findBound(nums, target, false);  // find right bound
+        
+        return ans;
+    }
+    
+    private int findBound(int[] nums, int target, boolean isFirst) {
         int left = 0;
         int right = nums.length - 1;
-
-        while (left <= right){
+        int result = -1;
+        
+        while (left <= right) {
             int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target){
-                ans[0] = mid;
-                right = mid -1;
-            }
-            else if(nums[mid] < target)
+            
+            if (nums[mid] == target) {
+                result = mid;
+                if (isFirst) {
+                    right = mid - 1;  // continue searching left
+                } else {
+                    left = mid + 1;   // continue searching right
+                }
+            } else if (nums[mid] < target) {
                 left = mid + 1;
-            else
+            } else {
                 right = mid - 1;
-        }
-        left = 0;
-        right = nums.length - 1;
-        while (left <= right){
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target){
-                ans[1] = mid;
-                left = mid + 1;
             }
-            else if(nums[mid] < target)
-                left = mid + 1;
-            else
-                right = mid - 1;
         }
-
-        return ans;
+        
+        return result;
     }
 }
